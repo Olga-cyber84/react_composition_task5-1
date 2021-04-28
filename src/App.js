a2b94b08153c55a3f948c05bf404e6a3e20fc608
Olga-cyber84 <doorsdream007@ya.ru>
2021-04-28 18:51:48 +0300

    project composition-cards added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
+import logo from './logo.svg';
 
 function App() {
+  const card1 = {
+    title: "Card title",
+    text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    button: "Go somewhere"
+  }
+  const card2 = {
+    title: "Special title treatment",
+    text: "with supporting text below as  natural lead-in to additional content.",
+    button: "Go somewhere"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Card {...card1}>
+      <img src={logo} className="card-img-top" alt="..."/>
+    </Card>
+    
+    <Card {...card2} />
+    </>
   );
 }
 
